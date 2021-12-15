45ff9ce91d2399233ce7b0e844f02dadcb0fea04
Kanishk Pachauri <krishna03012003@icloud.com>
2021-12-16 04:37:10 +0530

    learned for neuralnine #day1

---
+++ firstprogramm.c
@@ -0,0 +1,11 @@
+//
+// Created by Kanishk Pachauri on 16/12/21.
+//
+
+#include "stdio.h"
+
+int main(void){
+    printf("Hello World \n");
+    printf("This is Kanishk Pachauri \n");
+    return 0;
+}
